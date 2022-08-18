b4918f9a4373ea466c4207f83e55f49b27f1e3b1
Simran Makhijani <simranmakhijani55@gmail.com>
2022-08-18 14:33:24 +0530

    added project

---
+++ src/test/java/com/apps/keka/api/KekaApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apps.keka.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KekaApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
